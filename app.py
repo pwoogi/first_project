cb47c2f01ba28d54067987e18c63afc3cb93bddb
suzzeong <su981006@naver.com>
2022-07-14 00:32:53 +0900

    list 추가

--- app.py
+++ app.py
@@ -58,31 +58,60 @@ def todo_post():
     return jsonify({'msg': '등록 완료!'})
 
 
-# 완료함수를 실행시켰을 때 done 값을 1로 변경하기
-@app.route("/todo/done", methods=["POST"])
-def todo_done():
+# 운동기록 페이지
+@app.route('/workoutlog')
+def workoutlog():
+    return render_template('list.html')
+
+# DB에 운동기록 리스트 값 저장하기
+@app.route("/list", methods=["POST"])
+def list_post():
+    title_receive = request.form['title_give']
+    name_receive = request.form['name_give']
+    hour_receive = request.form['hour_give']
+    comment_receive = request.form['comment_give']
+
+
+    list_list = list(db.list.find({}, {'_id': False}))
+    count = len(list_list) + 1
+
+    doc = {
+        'num': count,
+        'name':name_receive,
+        'title':title_receive,
+        'hour':hour_receive,
+        'comment':comment_receive,
+        'done': 0
+    }
+    db.list.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!!'})
+
+# 운동기록 리스트의 값 가져와서 보여주기
+@app.route("/list", methods=["GET"])
+def list_get():
+    list_list = list(db.list.find({}, {'_id': False}))
+    return jsonify({'lists': list_list})
+
+# 완료함수를 실행시켰을 때 done 값을 1으로 변경하기
+@app.route("/list/done", methods=["POST"])
+def list_done():
     num_receive = request.form['num_give']
-    db.todo.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
-    # 해당 num의 done을 1로 변경해준다
-    return jsonify({'msg': '수행 완료!'})
+    db.list.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
+    return jsonify({'msg': '운동 기록 완료!'})
 
 # 취소함수를 실행시켰을 때 done 값을 0으로 변경하기
-@app.route("/todo/cancel", methods=["POST"])
-def todo_cancel():
+@app.route("/list/undone", methods=["POST"])
+def list_undone():
     num_receive = request.form['num_give']
-    db.todo.update_one({'num': int(num_receive)}, {'$set': {'done': 0}})
+    db.list.update_one({'num': int(num_receive)}, {'$set': {'done': 0}})
     return jsonify({'msg': '취소 완료!'})
 
-# 삭제함수를 실행시켰을때 DB 삭제하기
-@app.route("/todo/delete", methods=["POST"])
-def todo_delete():
+# 삭제함수를 실행시켰을 때 DB삭제
+@app.route("/list/delete", methods=["POST"])
+def list_delete():
     num_receive = request.form['num_give']
-    print("데이터 확인 : " + num_receive);
-    # print로 찍어봤을 때, 해당 순번이 정상적으로 들어옴
-    # 문제는 mongoDb에 있는 num은 int형이고, 내가 받아오려고 했던 num은 str형식이었음
-    db.todo.delete_one({'num': int(num_receive)})
-    # 문자열로 받은 데이터를 정수형으로 변환 시켜줘서 문제 해결
-    # int()를 이용한 형변환
+    db.list.delete_one({'num': int(num_receive)})
     return jsonify({'msg': '삭제 완료!'})
 
 if __name__ == '__main__':
